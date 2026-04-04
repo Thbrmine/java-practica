f83f414937b3546077c8a19ef93f1e12e1fa9509
Thbrmine <riottheot6@gmail.com>
2026-04-04 18:16:50 -0500

    5.11 Ciclos

--- ejeOviedo/5.11Ciclos/ep50.java
+++ ejeOviedo/5.11Ciclos/ep50.java
@@ -4,19 +4,19 @@ public class ep50 {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Ingrese un valor para n: ");
             int n = sc.nextInt();
-
+// Primer ciclo que recorre desde 1 hasta n
             for (int i = 1; i <= n; i++) {
                 long factorial = 1;
-                String proceso = ""; // Aquí guardaremos la cadena "1 * 2 * 3..."
-
+                String proceso = ""; 
+// Segundo ciclo que calcula el factorial de i y construye la cadena del proceso
                 for (int j = 1; j <= i; j++) {
                     factorial *= j;
                     
-                    // Si es el primer número, no ponemos el '*' antes
+                    // Si es el primer número, no se pone el 'x' antes
                     if (j == 1) {
                         proceso += j;
                     } else {
-                        proceso += " * " + j;
+                        proceso += " x " + j;
                     }
                 }
 
